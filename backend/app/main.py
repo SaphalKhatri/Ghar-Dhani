29b904db70c3b91341b7023f4cdd90373dccda2e
SaphalKhatri <Khatrisaphal11@gmail.com>
2026-09-17 22:17:49 +0545

    update on main

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI
 from app.database import engine,Base 
 
 from app.models.landlord import Landlord
+from app.models.lease import Lease
+from app.models.flat import Flat 
+from app.models.payment import Payment
+from app.model.tenant import Tenant
 
-
+from app.routes import landlord,flats,leases,payments,tenants
 
 app=FastAPI(title="rental management",description="api for rental",version="1.0.0")
 
